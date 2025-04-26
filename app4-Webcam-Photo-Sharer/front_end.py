d85ecfd17e3b64b2087848176ba756a3ade3d992
Varisht Raheja <varishtraheja@gmail.com>
2025-04-26 14:53:22 +0530

    Working on the project - part 1

--- app4-Webcam-Photo-Sharer/front_end.py
+++ app4-Webcam-Photo-Sharer/front_end.py
@@ -21,9 +21,7 @@ class MainApp(App):
     def build(self):
         box1 = BoxLayout(orientation='vertical')
         label1 = Label(text="Hello Kivy World!", font_size=50)
-        label2 = Label(text="Created a box layout.", font_size=30, color=(1, 0, 0, 1))
-        button1 = Button(text='A kivy Button', font_size=40, color=[1, 1, 0, 1])
+        button1 = Button(text='This is Kivy', font_size=40, color=[1, 1, 0, 1])
         box1.add_widget(label1)
-        box1.add_widget(label2)
         box1.add_widget(button1)
         return box1
