b31a5ef56cff4100ca9b110957b113af2e425b3e
Varisht Raheja <varishtraheja@gmail.com>
2025-05-31 11:43:36 +0530

    updated testing scenario

---
+++ app4-Webcam-Photo-Sharer/README.md
@@ -0,0 +1,16 @@
+# Webcam Photo Sharer
+Description: An app that starts the computer webcam, lets the user capture a photo
+and uploads the photo to the web and creates a sharable link.
+<br>
+Objects:
+1. Webcam:
+   - start()
+   - stop()
+   - capture()
+
+2. FileSharer:
+   - filepath
+   - api
+   - share()
+
+
--- app4-Webcam-Photo-Sharer/front_end.py
+++ app4-Webcam-Photo-Sharer/front_end.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.config import Config
 from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.image import Image
 
 # Layouts
@@ -10,18 +11,19 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 
 # Configuration
-Config.set('graphics', 'width', '1080')
-Config.set('graphics', 'height', '720')
+Config.set('graphics', 'width', '1920')
+Config.set('graphics', 'height', '1080')
 Config.set('graphics', 'resizable', '1')
 
 
 # Creating the objects
+class FirstScreen(Screen):
+    def search_image(self):
+        pass
+class RootWidget(ScreenManager):
+    pass
+
 class MainApp(App):
 
     def build(self):
-        box1 = BoxLayout(orientation='vertical')
-        label1 = Label(text="Hello Kivy World!", font_size=50)
-        button1 = Button(text='This is Kivy', font_size=40, color=[1, 1, 0, 1])
-        box1.add_widget(label1)
-        box1.add_widget(button1)
-        return box1
+        return RootWidget()
